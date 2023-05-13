becbfa07bc09694b3ec872f9df52358e109785da
svenjaklenke <svenja.klenke@gmx.de>
2023-05-13 14:09:52 +0200

    Add TeilnehmerCard and Stylesheets

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import esc from './images/esc.jpeg';
 
 import './App.css';
 
+import TeilnehmerGallery from "./TeilnehmerGallery";
+
 function App() {
   return (
     <div className="App">
         <img src={esc} alt="ESC"/>
+        <p>
+            <TeilnehmerGallery/>
+        </p>
     </div>
   );
 }
